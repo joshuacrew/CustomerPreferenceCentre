726010523bba43b74257fd120279f33019e3666d
Joshua Crew <Joshua.Crew@callcreditgroup.com>
2019-02-13 20:39:42 +0000

    Added Customer preference controller and initial e2e test

---
+++ Code/CustomerPreferenceCentreTests/CustomerPreferenceCentreTests/CustomerPreferenceController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using NUnit.Framework;
+using Shouldly;
+
+namespace CustomerPreferenceCentreTests
+{
+    public class CustomerPreferenceController
+    {
+        [Test]
+        public void ShouldReturnHelloWorld()
+        {
+            var result = new HttpClient().GetAsync(Urls.CustomerPreferenceUrl).Result;
+
+            result.Content.ReadAsStringAsync().Result.ShouldContain("Hello World");
+        }
+    }
+}
---
+++ Code/CustomerPreferenceCentreTests/CustomerPreferenceCentreTests/Urls.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CustomerPreferenceCentreTests
+{
+    public class Urls
+    {
+        public static readonly Uri CustomerPreferenceUrl = new Uri("https://localhost:44374/customer-preference");
+    }
+}
